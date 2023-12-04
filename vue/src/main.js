ffb7c7d76fb2e213c74f30ced2c15a88657ba777
meesvrh <mees.verhelpen@gmail.com>
2023-12-04 16:08:49 +0100

    feat(vue): add progress styling

--- vue/src/main.js
+++ vue/src/main.js
@@ -8,7 +8,20 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+const fmTheme = {
+    dark: true,
+    colors: {
+        primary: '#f0d611',
+      },
+}
+
 const vuetify = createVuetify({
+    theme: {
+        defaultTheme: 'fmTheme',
+        themes: {
+            fmTheme,
+        },
+    },
     components,
     directives,
 })
