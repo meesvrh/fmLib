7e8a2ac40973cd7825bc3e6cb572744d7b8332b9
Mees Verhelpen <mees.verhelpen@gmail.com>
2023-12-17 20:05:06 +0100

    refactor(server/player): player into class with metatable

--- wrappers/server/player.lua
+++ wrappers/server/player.lua
@@ -9,110 +9,92 @@ local function getPlayerByIdentifier(identifier)
     if not identifier then return end
     return ESX and ESX.GetPlayerFromIdentifier(identifier) or QB and QB.Functions.GetPlayerByCitizenId(identifier) or nil
 end
- 
----@param p table|number|string table: player, number: src, string: identifier 
+
+---@param p number|string number: src, string: identifier 
 function FM.player.get(p)
-    if not p then return end
-    return type(p) == 'number' and getPlayerBySrc(p) or type(p) == 'string' and getPlayerByIdentifier(p) or type(p) == 'table' and p or nil
+    p = type(p) == 'number' and getPlayerBySrc(p) or type(p) == 'string' and getPlayerByIdentifier(p) or nil
+    if not p or type(p) ~= 'table' then return end
+    
+    return setmetatable({ _fwp = p }, { __index = FM.player })
 end
 
----@param p table|number|string table: player, number: src, string: identifier
----@param moneyType? string default: Defaults.MONEY
 ---@param amount number
-function FM.player.addMoney(p, moneyType, amount)
-    p = FM.player.get(p)
-    moneyType = moneyType or Defaults.MONEY
-    if not p or not amount then return end
-
-    if ESX then p.addAccountMoney(moneyType, amount)
-    elseif QB then p.Functions.AddMoney(moneyType, amount) end
-end
-
----@param p table|number|string table: player, number: src, string: identifier
 ---@param moneyType? string default: Defaults.MONEY
----@param amount number
-function FM.player.removeMoney(p, moneyType, amount)
-    p = FM.player.get(p)
+function FM.player:addMoney(amount, moneyType)
     moneyType = moneyType or Defaults.MONEY
-    if not p or not amount then return end
+    if not amount then return end
 
-    if ESX then p.removeAccountMoney(moneyType, amount)
-    elseif QB then p.Functions.RemoveMoney(moneyType, amount) end
+    if ESX then self._fwp.addAccountMoney(moneyType, amount)
+    elseif QB then self._fwp.Functions.AddMoney(moneyType, amount) end
 end
 
----@param p table|number|string table: player, number: src, string: identifier
+---@param amount number
+---@param moneyType? string default: Defaults.MONEY
+function FM.player:removeMoney(amount, moneyType)
+    moneyType = moneyType or Defaults.MONEY
+    if not amount then return end
+
+    if ESX then self._fwp.removeAccountMoney(moneyType, amount)
+    elseif QB then self._fwp.Functions.RemoveMoney(moneyType, amount) end
+end
+
 ---@param moneyType? string default: Defaults.MONEY
 ---@return number amount
-function FM.player.getMoney(p, moneyType)
-    p = FM.player.get(p)
+function FM.player:getMoney(moneyType)
     moneyType = moneyType or Defaults.MONEY
-    if not p then return end
 
-    if ESX then return p.getAccount(moneyType).money
-    elseif QB then return p.PlayerData.money[moneyType] end
+    if ESX then return self._fwp.getAccount(moneyType).money
+    elseif QB then return self._fwp.PlayerData.money[moneyType] end
 end
 
----@param p table|number|string table: player, number: src, string: identifier
 ---@return { name: string, label: string, grade: number, gradeLabel: string } job
-function FM.player.getJob(p)
-    p = FM.player.get(p)
-    if not p then return end
-
+function FM.player:getJob()
     if ESX then
         return {
-            name = p.job.name,
-            label = p.job.label,
-            grade = p.job.grade,
-            gradeLabel = p.job.grade_label
+            name = self._fwp.job.name,
+            label = self._fwp.job.label,
+            grade = self._fwp.job.grade,
+            gradeLabel = self._fwp.job.grade_label
         }
     elseif QB then
         return {
-            name = p.PlayerData.job.name,
-            label = p.PlayerData.job.label,
-            grade = p.PlayerData.job.grade.level,
-            gradeLabel = p.PlayerData.job.grade.name
+            name = self._fwp.PlayerData.job.name,
+            label = self._fwp.PlayerData.job.label,
+            grade = self._fwp.PlayerData.job.grade.level,
+            gradeLabel = self._fwp.PlayerData.job.grade.name
         }
     end
 end
 
----@param p table|number|string table: player, number: src, string: identifier
 ---@return { name: string, label: string, grade: number, gradeLabel: string } gang
-function FM.player.getGang(p)
-    p = FM.player.get(p)
-    if not p then return end
-
-    if ESX then return FM.getJob(p)
+function FM.player:getGang()
+    if ESX then return FM.getJob()
     elseif QB then
         return {
-            name = p.PlayerData.gang.name,
-            label = p.PlayerData.gang.label,
-            grade = p.PlayerData.gang.grade.level,
-            gradeLabel = p.PlayerData.gang.grade.name
+            name = self._fwp.PlayerData.gang.name,
+            label = self._fwp.PlayerData.gang.label,
+            grade = self._fwp.PlayerData.gang.grade.level,
+            gradeLabel = self._fwp.PlayerData.gang.grade.name
         }
     end
 end
 
----@param src number
 ---@return boolean
-function FM.player.isAdmin(src)
-    if not src then return end
-
+function FM.player:isAdmin()
     if ESX then
-        return FM.player.get(src).getGroup() == "admin"
+        return self._fwp.getGroup() == "admin"
     elseif QB then
-        return QB.Functions.HasPermission(src, "god")
+        return QB.Functions.HasPermission(self._fwp.source, "god")
     end
 end
 
----@param p table|number|string table: player, number: src, string: identifier
 ---@param item string
 ---@return { name: string, label: string, amount: number } item
-function FM.player.getItem(p, item)
-    p = FM.player.get(p)
-    if not p or not item then return end
+function FM.player:getItem(item)
+    if not item then return end
 
     if ESX then
-        item = p.getInventoryItem(item)
+        item = self._fwp.getInventoryItem(item)
         if not item then return end
 
         return {
@@ -121,7 +103,7 @@ function FM.player.getItem(p, item)
             amount = item.count
         }
     elseif QB then
-        item = p.Functions.GetItemByName(item)
+        item = self._fwp.Functions.GetItemByName(item)
         if not item then return end
 
         return {
@@ -132,50 +114,40 @@ function FM.player.getItem(p, item)
     end
 end
 
----@param p table|number|string table: player, number: src, string: identifier
 ---@param item string
 ---@param amount number
 ---@return boolean
-function FM.player.hasItemAmount(p, item, amount)
-    p = FM.player.get(p)
-    if not p or not item then return end
+function FM.player:hasItemAmount(item, amount)
+    if not item then return end
 
-    item = FM.player.getItem(p, item)
+    item = self:getItem(item)
     return item and item.amount >= amount or false
 end
 
----@param p table|number|string table: player, number: src, string: identifier
 ---@param item string
 ---@param amount number
-function FM.player.addItem(p, item, amount)
-    p = FM.player.get(p)
-    if not p or not item or not amount then return end
+function FM.player:addItem(item, amount)
+    if not item or not amount then return end
 
-    if ESX then p.addInventoryItem(item, amount)
-    elseif QB then p.Functions.AddItem(item, amount) end
+    if ESX then self._fwp.addInventoryItem(item, amount)
+    elseif QB then self._fwp.Functions.AddItem(item, amount) end
 end
 
----@param p table|number|string table: player, number: src, string: identifier
 ---@param item string
 ---@param amount number
-function FM.player.removeItem(p, item, amount)
-    p = FM.player.get(p)
-    if not p or not item or not amount then return end
+function FM.player:removeItem(item, amount)
+    if not item or not amount then return end
 
-    if ESX then p.removeInventoryItem(item, amount)
-    elseif QB then p.Functions.RemoveItem(item, amount) end
+    if ESX then self._fwp.removeInventoryItem(item, amount)
+    elseif QB then self._fwp.Functions.RemoveItem(item, amount) end
 end
 
----@param p table|number|string table: player, number: src, string: identifier
 ---@return { [string]: { amount: number, label: string } } inventory
-function FM.player.getInventoryItems(p)
-    p = FM.player.get(p)
-    if not p then return end
-
+function FM.player:getInventoryItems()
     local inventory = {}
 
     if ESX then
-        local items = p.getInventory()
+        local items = self._fwp.getInventory()
         for _, v in pairs(items) do
             inventory[v.name] = {
                 amount = v.count,
@@ -183,7 +155,7 @@ function FM.player.getInventoryItems(p)
             }
         end
     elseif QB then
-        local items = p.PlayerData.items
+        local items = self._fwp.PlayerData.items
         for _, v in pairs(items) do
             inventory[v.name] = {
                 amount = v.amount,
@@ -195,44 +167,28 @@ function FM.player.getInventoryItems(p)
     return inventory
 end
 
----@param p table|number|string table: player, number: src, string: identifier
 ---@return string firstName
-function FM.player.getFirstName(p)
-    p = FM.player.get(p)
-    if not p then return end
-
-    if ESX then return p.getName().split(' ')[1]
-    elseif QB then return p.PlayerData.charinfo.firstname end
+function FM.player:getFirstName()
+    if ESX then return self._fwp.getName().split(' ')[1]
+    elseif QB then return self._fwp.PlayerData.charinfo.firstname end
 end
 
----@param p table|number|string table: player, number: src, string: identifier
 ---@return string lastName
-function FM.player.getLastName(p)
-    p = FM.player.get(p)
-    if not p then return end
-
-    if ESX then return p.getName().split(' ')[2]
-    elseif QB then return p.PlayerData.charinfo.lastname end
+function FM.player:getLastName()
+    if ESX then return self._fwp.getName().split(' ')[2]
+    elseif QB then return self._fwp.PlayerData.charinfo.lastname end
 end
 
----@param p table|number|string table: player, number: src, string: identifier
 ---@return string fullName
-function FM.player.getFullName(p)
-    p = FM.player.get(p)
-    if not p then return end
-
-    if ESX then return p.getName()
-    elseif QB then return p.PlayerData.charinfo.firstname .. ' ' .. p.PlayerData.charinfo.lastname end
+function FM.player:getFullName()
+    if ESX then return self._fwp.getName()
+    elseif QB then return self._fwp.PlayerData.charinfo.firstname .. ' ' .. self._fwp.PlayerData.charinfo.lastname end
 end
 
----@param p table|number|string table: player, number: src, string: identifier
 ---@return string identifier
-function FM.player.getIdentifier(p)
-    p = FM.player.get(p)
-    if not p then return end
-
-    if ESX then return p.getIdentifier()
-    elseif QB then return p.PlayerData.citizenid end
+function FM.player:getIdentifier()
+    if ESX then return self._fwp.getIdentifier()
+    elseif QB then return self._fwp.PlayerData.citizenid end
 end
 
 FM.p = FM.player
