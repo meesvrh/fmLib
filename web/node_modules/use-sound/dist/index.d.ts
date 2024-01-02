a3e237e1d821d273b6dda641468769b25b189957
Mees Verhelpen <mees.verhelpen@gmail.com>
2024-01-02 22:59:21 +0100

    fully removal due to annoying merge

--- web/node_modules/use-sound/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { HookOptions, ReturnedValue } from './types';
-export default function useSound<T = any>(src: string | string[], { id, volume, playbackRate, soundEnabled, interrupt, onload, ...delegated }?: HookOptions<T>): ReturnedValue;
-export { useSound };
