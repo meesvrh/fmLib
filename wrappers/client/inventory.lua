689d086108eb6f8c415b38e1dc6cd4ba4e2ccb4c
Mees Verhelpen <mees.verhelpen@gmail.com>
2024-04-08 18:01:22 +0200

    feat(wrappers/client/inventory): add getItems & getItemsAmounts

--- wrappers/client/inventory.lua
+++ wrappers/client/inventory.lua
@@ -10,4 +10,59 @@ function FM.inventory.open(type, id)
     end
 end
 
+function FM.inventory.getItemsAmounts()
+    local items = FM.inventory.getItems()
+    local amounts = {}
+
+    for i = 1, #items do
+        local item = items[i]
+        if amounts[item.name] then
+            amounts[item.name] = amounts[item.name] + item.amount
+        else
+            amounts[item.name] = item.amount
+        end
+    end
+
+    return amounts
+end
+
+-- TODO: test for qbcore
+function FM.inventory.getItems()
+    local inventory = {}
+
+    if OXInv then
+        local items = OXInv:GetPlayerItems()
+        for i = 1, #items do
+            local item = items[i]
+            inventory[#inventory+1] = {
+                name = item.name,
+                label = item.label,
+                amount = item.count
+            }
+        end
+    elseif ESX then
+        local items = ESX.GetPlayerData().inventory
+        for i = 1, #items do
+            local item = items[i]
+            inventory[#inventory+1] = {
+                name = item.name,
+                label = item.label,
+                amount = item.count
+            }
+        end
+    elseif QB then
+        local items = QB.Functions.GetPlayerData().items
+        for i = 1, #items do
+            local item = items[i]
+            inventory[#inventory+1] = {
+                name = item.name,
+                label = item.label,
+                amount = item.amount
+            }
+        end
+    end
+
+    return inventory
+end
+
 FM.inv = {}
