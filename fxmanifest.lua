6326f4a67eea572d20a964d2deafe96bcce32d42
Mees Verhelpen <mees.verhelpen@gmail.com>
2024-10-24 01:17:20 +0200

    v1.11.4

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,7 +11,7 @@ games { 'gta5' }
 author 'meesvrh'
 repository 'fmLib'
 description 'A library for FiveM developers that wraps multiple frameworks, resources and modules.'
-version '1.11.3'
+version '1.11.4'
 
 shared_script {
   'settings.lua',
