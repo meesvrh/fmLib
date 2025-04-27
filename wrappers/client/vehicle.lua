891f1233855b1f7b67d24a4c04acd7e004f9b3ad
GR-Scripts <155775715+GR-Scripts@users.noreply.github.com>
2025-04-28 00:09:34 +0200

    Update vehicle.lua

--- wrappers/client/vehicle.lua
+++ wrappers/client/vehicle.lua
@@ -127,8 +127,8 @@ function FM.vehicle.setFuel(vehicle, fuelLvl)
         MYFUEL:SetFuel(vehicle, fuelLvl)
     elseif LJ_FUEL then
         LJ_FUEL:SetFuel(vehicle, fuelLvl)
-    elseif MELLONS_FUEL then
-         MELLONS_FUEL:SetFuel(vehicle, fuelLvl)
+    elseif MELONS_FUEL then
+         MELONS_FUEL:SetFuel(vehicle, fuelLvl)
     else
         SetVehicleFuelLevel(vehicle, fuelLvl)
     end
