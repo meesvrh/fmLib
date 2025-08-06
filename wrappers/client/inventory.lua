2185f4e0463876155a619e34278079e0e734935a
GR-Scripts <gamer.raymond23@gmail.com>
2025-08-06 07:21:53 +0200

    aa

---
+++ wrappers/client/inventory.lua
@@ -0,0 +1,140 @@
+FM.inventory = {}
+
+local function isNewQBInv()
+    local version = GetResourceMetadata(Resources.QBInv or 'qb-inventory', 'version', 0)
+    if not version then return false end
+
+    local vNums = {}
+
+    for num in version:gmatch("(%d+)") do
+        vNums[#vNums + 1] = tonumber(num)
+    end
+
+    return vNums and vNums[1] >= 2
+end
+
+local function isNewPSInv()
+    local version = GetResourceMetadata('ps-inventory', 'version', 0)
+    if not version then return false end
+
+    local vNums = {}
+
+    for num in version:gmatch("(%d+)") do
+        vNums[#vNums + 1] = tonumber(num)
+    end
+
+    return vNums and vNums[1] >= 2
+end
+
+--- Currently only working for qb-inventory
+---@param type 'otherplayer'
+---@param id string | number
+function FM.inventory.open(type, id)
+    if not type or not id then return end
+
+    if QBInv then
+        TriggerServerEvent("inventory:server:OpenInventory", type, id)
+    end
+end
+
+---@param stashId string | number
+---@param owner? string | boolean
+---@param weight? number
+---@param slots? number
+function FM.inventory.openStash(stashId, owner, weight, slots)
+    if not stashId then FM.console.err('No stash ID provided') return end
+
+    if OXInv then
+        OXInv:openInventory('stash', { id = stashId, owner = owner })
+    elseif QBInv or QSInv or PSInv or CODEMInv then
+        if (QBInv and isNewQBInv()) or (PSInv and isNewPSInv()) then
+            TriggerServerEvent('fm:internal:openStash', stashId, owner, weight, slots)
+        else
+            if QSInv then
+                QSInv:RegisterStash(stashId, slots, weight)
+            end
+            
+            TriggerServerEvent('inventory:server:OpenInventory', 'stash', stashId, {
+                maxweight = weight,
+                slots = slots,
+            })
+            TriggerEvent('inventory:client:SetCurrentStash', stashId)
+        end
+    elseif JPRInv then
+        TriggerServerEvent('fm:internal:openStash', stashId, owner, weight, slots)
+    else
+        FM.console.err("No inventory found for opening stash")
+    end
+end
+
+function FM.inventory.getItemsAmounts()
+    local items = FM.inventory.getItems()
+    local amounts = {}
+
+    for slot, item in pairs(items) do
+        if amounts[item.name] then
+            amounts[item.name] = amounts[item.name] + item.amount
+        else
+            amounts[item.name] = item.amount
+        end
+    end
+
+    return amounts
+end
+
+-- TODO: test for qbcore
+function FM.inventory.getItems()
+    local inventory = {}
+
+    if OXInv then
+        local items = OXInv:GetPlayerItems()
+        for slot, item in pairs(items) do
+            inventory[slot] = {
+                name = item.name,
+                label = item.label,
+                amount = item.count
+            }
+        end
+    elseif ESX then
+        local items = ESX.GetPlayerData().inventory
+        for slot, item in pairs(items) do
+            inventory[slot] = {
+                name = item.name,
+                label = item.label,
+                amount = item.count
+            }
+        end
+    elseif QB then
+        local items = QB.Functions.GetPlayerData().items
+        for slot, item in pairs(items) do
+            inventory[slot] = {
+                name = item.name,
+                label = item.label,
+                amount = item.amount
+            }
+        end
+    end
+
+    return inventory
+end
+
+---@param state boolean
+function FM.inventory.setWeaponWheel(state)
+    if OXInv then
+        OXInv:weaponWheel(state)
+    elseif QSInv then
+        QSInv:WeaponWheel(state)
+    end
+end
+
+---@param item string
+function FM.inventory.hasItem(item)
+    if ESX then
+        local has = ESX.SearchInventory(item, 1)
+        return has and has > 0
+    elseif QB then
+        return QB.Functions.HasItem(item)
+    end
+end
+
+FM.inv = {}
---
+++ wrappers/client/vehicle.lua
@@ -0,0 +1,181 @@
+FM.vehicle = {}
+
+---@param vehicle number
+function FM.vehicle.getPlate(vehicle)
+    if not vehicle then return end
+
+    if ESX then
+        return ESX.Math.Trim(GetVehicleNumberPlateText(vehicle))
+    elseif QB then
+        return QB.Functions.GetPlate(vehicle)
+    end
+end
+
+---@param vehicle number
+function FM.vehicle.giveKeys(vehicle)
+    if not vehicle then return end
+    local plate = FM.vehicle.getPlate(vehicle)
+    local model = GetDisplayNameFromVehicleModel(GetEntityModel(vehicle))
+
+    if QBVehKeys then
+        TriggerEvent("vehiclekeys:client:SetOwner", plate)
+        TriggerServerEvent('qb-vehiclekeys:server:AcquireVehicleKeys', plate)
+    elseif CDGarage then
+        TriggerEvent('cd_garage:AddKeys', plate)
+    elseif okokGarage then
+        TriggerServerEvent('okokGarage:GiveKeys', plate)
+    elseif QSVehKeys then
+        QSVehKeys:GiveKeys(plate, model, true)
+    elseif RenewedVehKeys then
+        RenewedVehKeys:addKey(plate)
+    elseif WASABI_CARLOCK then
+        WASABI_CARLOCK:GiveKey(plate)
+    elseif TGIANN_HOTWIRE then
+        TGIANN_HOTWIRE:GiveKeyPlate(plate, true)
+    elseif MM_CARKEYS then
+        MM_CARKEYS:GiveKey(plate)
+    elseif MRNEWBVEHICLEKEYS then
+        MRNEWBVEHICLEKEYS:GiveKeys(vehicle)
+    elseif ISVehicleKeys then
+        ISVehicleKeys:GiveKey(plate)
+    elseif FAST_VEHICLEKEYS then
+        FAST_VEHICLEKEYS:GiveKey(plate)
+    elseif FILO_VEHICLEKEYS then
+        FILO_VEHICLEKEYS:GiveKeys(plate)
+    elseif QBX_VEHICLEKEYS then
+        QBX_VEHICLEKEYS:GiveKeys(vehicle)
+    end
+end
+
+---@param vehicle number
+function FM.vehicle.removeKeys(vehicle)
+    if not vehicle then return end
+    local plate = FM.vehicle.getPlate(vehicle)
+    local model = GetDisplayNameFromVehicleModel(GetEntityModel(vehicle))
+
+    if QBVehKeys then
+        TriggerEvent("vehiclekeys:client:RemoveKeys", plate)
+    elseif okokGarage then
+        TriggerServerEvent('okokGarage:RemoveKeys', plate)
+    elseif QSVehKeys then
+        QSVehKeys:RemoveKeys(plate, model)
+    elseif RenewedVehKeys then
+        RenewedVehKeys:removeKey(plate)
+    elseif WASABI_CARLOCK then
+        WASABI_CARLOCK:RemoveKey(plate)
+    elseif MM_CARKEYS then
+        MM_CARKEYS:RemoveKey(plate)
+    elseif MRNEWBVEHICLEKEYS then
+        MRNEWBVEHICLEKEYS:RemoveKeys(vehicle)
+    elseif QBX_VEHICLEKEYS then
+        QBX_VEHICLEKEYS:RemoveKeys(vehicle)
+    end
+end
+
+---@param vehicle number
+function FM.vehicle.hasKey(vehicle)
+    if not vehicle then return end
+    local plate = FM.vehicle.getPlate(vehicle)
+
+    if QBVehKeys then
+        QBVehKeys:hasKey(plate)
+    elseif CDGarage then
+        return CDGarage:DoesPlayerHaveKeys(plate)
+    elseif QSVehKeys then
+        return QSVehKeys:GetServerKey(plate)
+    elseif RenewedVehKeys then
+        return RenewedVehKeys:hasKey(plate)
+    elseif WASABI_CARLOCK then
+        return WASABI_CARLOCK:HasKey(plate)
+    elseif TGIANN_HOTWIRE then
+        return TGIANN_HOTWIRE:HasKeyPlate(plate)
+    elseif MRNEWBVEHICLEKEYS then
+        return MRNEWBVEHICLEKEYS:HaveKeys(vehicle)
+    elseif QBX_VEHICLEKEYS then
+        return QBX_VEHICLEKEYS:HasKeys(vehicle)
+    end
+end
+
+
+---@param vehicle number
+---@param fuelLvl number
+function FM.vehicle.setFuel(vehicle, fuelLvl)
+    if not vehicle or fuelLvl == nil then return end
+
+    fuelLvl = fuelLvl + 0.0
+
+    if OXFUEL then
+        Entity(vehicle).state.fuel = fuelLvl
+    elseif LEGACYFUEL then
+        LEGACYFUEL:SetFuel(vehicle, fuelLvl)
+    elseif CDN_FUEL then
+        CDN_FUEL:SetFuel(vehicle, fuelLvl)
+    elseif RENEWED_FUEL then
+        RENEWED_FUEL:SetFuel(vehicle, fuelLvl)
+    elseif QBFUEL then
+        QBFUEL:SetFuel(vehicle, fuelLvl)
+    elseif LC_FUEL then
+        LC_FUEL:SetFuel(vehicle, fuelLvl)
+    elseif PS_FUEL then
+        PS_FUEL:SetFuel(vehicle, fuelLvl)
+    elseif RCORE_FUEL then
+        RCORE_FUEL:SetFuel(vehicle, fuelLvl)
+    elseif QS_FUEL then
+        QS_FUEL:SetFuel(vehicle, fuelLvl)
+    elseif ND_FUEL then
+        ND_FUEL:SetFuel(vehicle, fuelLvl)
+    elseif BIGDADDY_FUEL then
+        BIGDADDY_FUEL:SetFuel(vehicle, fuelLvl)
+    elseif GKS_FUEL then
+        GKS_FUEL:SetFuel(vehicle, fuelLvl)
+    elseif RIP_FUEL then
+        RIP_FUEL:SetFuel(vehicle, fuelLvl)
+    elseif MYFUEL then
+        MYFUEL:SetFuel(vehicle, fuelLvl)
+    elseif LJ_FUEL then
+        LJ_FUEL:SetFuel(vehicle, fuelLvl)
+    elseif MELONS_FUEL then
+         MELONS_FUEL:SetFuel(vehicle, fuelLvl)
+    elseif HRS_FUEL then
+         HRS_FUEL:SetFuel(vehicle, fuelLvl)
+    else
+        SetVehicleFuelLevel(vehicle, fuelLvl)
+    end
+end
+
+---@param garageId string | number The garage ID
+function FM.vehicle.storeInGarage(garageId)
+    if RXGARAGE then
+        RXGARAGE:ParkVehicle("House Garage ("..tostring(garageId)..")", 'garage', 'car')
+    elseif OKOKG then
+        TriggerEvent("okokGarage:StoreVehiclePrivate")
+    elseif JGGARAGE then
+        TriggerEvent("jg-advancedgarages:client:store-vehicle", "House: "..tostring(garageId), "car")
+    elseif CODEMG then
+        TriggerEvent("codem-garage:storeVehicle", garageId)
+    elseif CDGarage then
+        TriggerEvent('cd_garage:StoreVehicle_Main', 1, false, false)
+    else
+        FM.console.err("No garage resource found")
+    end
+end
+
+---@param garageId string | number The garage ID
+---@param coords vector4 The coordinates for the garage
+function FM.vehicle.openGarage(garageId, coords)
+    if RXGARAGE then
+        RXGARAGE:OpenGarage("House Garage ("..tostring(garageId)..")", 'garage', 'car', coords)
+    elseif OKOKG then
+        TriggerEvent("okokGarage:OpenPrivateGarageMenu", vector3(coords.x, coords.y, coords.z), coords.w)
+    elseif JGGARAGE then
+        TriggerEvent("jg-advancedgarages:client:open-garage", "House: "..tostring(garageId), "car", coords)
+    elseif CODEMG then
+        TriggerEvent("codem-garage:openHouseGarage", garageId)
+    elseif CDGarage then
+        -- Choose either 'quick' or 'inside' in the 1st argument.
+        -- Replace nil with '10cargarage_shell' or '40cargarage_shell' in the 2nd argument.
+        TriggerEvent('cd_garage:PropertyGarage', 'quick', nil)
+    else
+        FM.console.err("No garage resource found")
+    end
+end
