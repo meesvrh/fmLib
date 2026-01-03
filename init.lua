e95a72415b1fa713e2e8e93304b936be3e7f42fc
meesvrh <mees.verhelpen@gmail.com>
2026-01-03 15:48:46 +0100

    removal of outdated files

--- init.lua
+++ init.lua
@@ -44,15 +44,6 @@ function Update(str, ...)
     end
 end
 
--- function FM:construct()
---     setmetatable({}, self)
---     self.__index = self
---     return self
--- end
--- exports('new', function()
---     return FM:construct()
--- end)
-
 if not IsDuplicityVersion() then
     RegisterNUICallback('fetchSettings', function(data, cb)
         cb({
