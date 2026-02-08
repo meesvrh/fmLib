0d527df044fda7ced9fb2d238c040a1718286cf0
meesvrh <mees.verhelpen@gmail.com>
2026-02-08 23:26:31 +0100

    feat: add codem getImgDirectory

--- adapters/client/inventory/codem.lua
+++ adapters/client/inventory/codem.lua
@@ -13,4 +13,8 @@ function adapter.openStash(stashId, owner, weight, slots)
     TriggerEvent('inventory:client:SetCurrentStash', stashId)
 end
 
+function adapter.getImgDirectory()
+    return 'codem-Inventory/html/itemimages/'
+end
+
 FM_Adapter_client_inventory_codem = adapter
